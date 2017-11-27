a63d299a3330b5c549af59cd9c49825f00166e8b
quangnle <mnest2409@yahoo.com>
2017-11-27 22:01:29 +0700

    commit

---
+++ StrategyChess/AI/Computer.cs
@@ -0,0 +1,32 @@
+﻿using StrategyChessCore.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    public class Computer
+    {
+        public Computer(int boardHeight, int boardWidth)
+        {
+
+        }
+
+        public Node GetNextMove(string teamName, Team upperTeam, Team lowerTeam)
+        {
+            return null;
+        }
+
+        private List<Node> GenNextNodes(Node node)
+        {
+            return null;
+        }
+
+        private int Heuristic(Node node)
+        {
+            return -1;
+        }
+    }
+}
---
+++ StrategyChess/AI/Node.cs
@@ -0,0 +1,19 @@
+﻿using StrategyChessCore.Definitions;
+using StrategyChessCore.Definitions.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI
+{
+    public class Node
+    {
+        public int Heuristic { get; set; }
+        public IUnit SelectedUnit { get; set; }
+        public Team OwnTeam { get; set; }
+        public Team Oponent { get; set; }
+        public List<Node> Children { get; set; }
+    }
+}
